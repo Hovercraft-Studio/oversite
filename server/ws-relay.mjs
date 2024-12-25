44a4865e760e2163d5aed78dcdc61a54361111c7
Justin Gitlin <cacheflowe@cacheflowe.com>
2024-12-25 16:54:07 -0500

    Clean up attaching sender to ws clients on server side by using newly-supported ws:// URL query param scheme

--- server/ws-relay.mjs
+++ server/ws-relay.mjs
@@ -48,18 +48,18 @@ eventLog(
 
 // listen for new connections
 wsServer.on("connection", (connection, request, client) => {
-  // Log new connection and add metadata to connection object
-  eventLog("Client joined - We have " + wsServer.clients.size + " users");
-  connection.startTime = Date.now();
-  // get sender from searchParams
+  // get sender from searchParams and add metadata to connection object
   // format is: /ws?sender=MONITOR_DEMO
   let fullReqURL = `ws://localhost:${wssPort}${request.url}`;
   const searchParams = new URL(fullReqURL).searchParams;
-  console.log("Client connection URL", fullReqURL);
   const sender = searchParams.get("sender");
   connection.senderID = sender;
   connection.senderID ??= "unknown";
-  console.log("new connection with sender:", connection.senderID);
+  connection.startTime = Date.now();
+  // Log new connection
+  eventLog(
+    `Client joined from ${fullReqURL} - We have ${wsServer.clients.size} users`
+  );
 
   // handle incoming messages
   connection.on("message", (message, isBinary) => {
@@ -68,8 +68,6 @@ wsServer.on("connection", (connection, request, client) => {
     // relay incoming message to all clients
     wsServer.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        // attach sender id to connection
-        matchClientWithSenderId(connection, client, message);
         // relay message
         client.send(message, { binary: isBinary });
       }
@@ -81,26 +79,6 @@ wsServer.on("connection", (connection, request, client) => {
     eventLog("Client left - We have " + wsServer.clients.size + " users");
   });
 });
-
-// if message is coming from own connection, check for send so we can attach to connection
-function matchClientWithSenderId(connection, client, message) {
-  if (client == connection) {
-    // only attach sender if we haven't already
-    if (!client.senderID) {
-      if (message.indexOf("sender")) {
-        try {
-          let json = JSON.parse(message);
-          if (json.sender) {
-            // attach sender to client
-            client.senderID = json.sender;
-          }
-        } catch (error) {
-          console.error("Error parsing sender ID:", error);
-        }
-      }
-    }
-  }
-}
 
 export default wsServer;
 export { wsServer, eventLog };
