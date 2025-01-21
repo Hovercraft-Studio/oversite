43200a03d64ea55798e692d8066e297f3163434e
Olivia Ruiz-Knott <livvy@hovercraftstudio.com>
2025-01-21 14:52:32 -0700

    use _register-components instead of importing the files in app.js

---
+++ shared/js/haxademic.js/components/_register-components.js
@@ -0,0 +1,18 @@
+// Import to trigger self-executing register() functions inside the component classes
+import "./app-store-button.js";
+import "./app-store-debug.js";
+import "./app-store-element.js";
+import "./app-store-health.js";
+import "./app-store-heartbeat.js";
+import "./app-store-init.js";
+import "./app-store-number.js";
+import "./app-store-slider.js";
+import "./app-store-table-css.js";
+import "./app-store-table.js";
+import "./app-store-textfield.js";
+import "./date-year.js";
+import "./event-log-view.js";
+import "./websocket-indicator.js";
+
+import "./app-store-clients.js";
+import "./app-store-event-table.js";
