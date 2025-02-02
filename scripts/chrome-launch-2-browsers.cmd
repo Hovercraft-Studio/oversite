917d6f04201a4ade9f10ce9d568262cfc82523a1
Justin Gitlin <cacheflowe@cacheflowe.com>
2025-02-02 14:16:33 -0500

    Note escape character and quotes in shell script URLs

--- scripts/chrome-launch-2-browsers.cmd
+++ scripts/chrome-launch-2-browsers.cmd
@@ -1,4 +1,8 @@
 @REM taskkill /F /T /IM chrome.exe
 set CHROME_BASE_PATH="C:\\Program Files\\Chromium\\Application\\"
-node run-chrome.mjs --chrome-path %CHROME_BASE_PATH% --url http://localhost:3002/ --fullscreen true --location 10,10 --screen-num 1
-node run-chrome.mjs --chrome-path %CHROME_BASE_PATH% --url http://localhost:3002/ --fullscreen true --location 10,2100 --screen-num 2
+
+@REM !!! Please note the "^&" escape character to start the hash string. Also, please wrap the URL with quotes.
+@REM !!! This is a weird issue with windows shell scripts, and the hash will be ignored without it.
+
+node run-chrome.mjs --chrome-path %CHROME_BASE_PATH% --url "http://localhost:3002/#^&wsURL=ws://localhost:3001/ws" --fullscreen true --location 10,10 --screen-num 1
+node run-chrome.mjs --chrome-path %CHROME_BASE_PATH% --url "http://localhost:3002/#^&wsURL=ws://localhost:3001/ws" --fullscreen true --location 10,2100 --screen-num 2
