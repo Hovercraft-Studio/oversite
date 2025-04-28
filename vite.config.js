93164c62b57ba3f26705aa9de8adfd4dcd69a2b1
Justin Gitlin <cacheflowe@cacheflowe.com>
2025-04-28 11:54:07 -0400

    Fix link to dashboard example for build/prod

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default {
         app_store_demo: resolve(__dirname, "app-store-demo/index.html"),
         app_store_monitor: resolve(__dirname, "app-store-monitor/index.html"),
         dashboard: resolve(__dirname, "dashboard/index.html"),
-        dashboard_example: resolve(__dirname, "examples/dashboard/js/index.html"),
+        dashboard_example: resolve(__dirname, "examples/dashboard/js-frontend/index.html"),
       },
     },
   },
