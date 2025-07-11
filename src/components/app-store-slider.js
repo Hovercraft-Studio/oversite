f8134fe8bccd3f0f5791940b3507181ddf7697ac
Olivia Ruiz-Knott <livvy@hovercraftstudio.com>
2025-07-11 15:45:48 -0600

    Refactor store value initialization in app-store components for consistency

--- src/components/app-store-slider.js
+++ src/components/app-store-slider.js
@@ -3,15 +3,16 @@ import AppStoreElement from "./app-store-element.js";
 class AppStoreSlider extends AppStoreElement {
   initStoreListener() {
     this.input = this.el.querySelector("input");
-    this.input.addEventListener("input", (e) => {
-      _store.set(this.storeKey, parseFloat(e.target.value), true);
-    });
+
     this.input.setAttribute("min", this.getAttribute("min") || 0);
     this.input.setAttribute("max", this.getAttribute("max") || 1);
     this.input.setAttribute("step", this.getAttribute("step") || 0.001);
-    this.input.value = this.hasAttribute("value")
-      ? parseFloat(this.getAttribute("value"))
-      : 0;
+
+    this.input.value = parseFloat(this.storeValue) || 0;
+
+    this.input.addEventListener("input", (e) => {
+      _store.set(this.storeKey, parseFloat(e.target.value), true);
+    });
 
     super.initStoreListener();
   }
